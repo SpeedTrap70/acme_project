8c821ffa60f5ddccc1c98dd6a93716a2977313d9
Давид Шаганц <speedtrap70@yandex.ru>
2023-08-29 22:03:15 +0300

    backup before CBV

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,3 +1,5 @@
+# Импортируем класс пагинатора.
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
 
 
@@ -21,7 +23,10 @@ def birthday(request, pk=None):
         instance = None
     # Передаём в форму либо данные из запроса, либо None. 
     # В случае редактирования прикрепляем объект модели.
-    form = BirthdayForm(request.POST or None, instance=instance)
+    form = BirthdayForm(request.POST or None,
+                        # Файлы, переданные в запросе, указываются отдельно.
+                        files=request.FILES or None,
+                        instance=instance)
     # Создаём словарь контекста сразу после инициализации формы.
     context = {'form': form}
     # Если форма валидна...
@@ -38,11 +43,21 @@ def birthday(request, pk=None):
 
 
 def birthday_list(request):
-    # Получаем все объекты модели Birthday из БД.
-    birthdays = Birthday.objects.all()
-    # Передаём их в контекст шаблона.
-    context = {'birthdays': birthdays}
-    return render(request, 'birthday/birthday_list.html', context)
+    # Получаем список всех объектов с сортировкой по id.
+    birthdays = Birthday.objects.order_by('id')
+    # Создаём объект пагинатора с количеством 10 записей на страницу.
+    paginator = Paginator(birthdays, 2)
+
+    # Получаем из запроса значение параметра page.
+    page_number = request.GET.get('page')
+    # Получаем запрошенную страницу пагинатора. 
+    # Если параметра page нет в запросе или его значение не приводится к числу,
+    # вернётся первая страница.
+    page_obj = paginator.get_page(page_number)
+    # Вместо полного списка объектов передаём в контекст 
+    # объект страницы пагинатора
+    context = {'page_obj': page_obj}
+    return render(request, 'birthday/birthday_list.html', context) 
 
 
 def delete_birthday(request, pk):
