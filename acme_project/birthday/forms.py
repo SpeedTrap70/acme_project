34c1ba47aac57e9c77bed6f2938e72dcf1a3e0e8
Давид Шаганц <speedtrap70@yandex.ru>
2023-08-30 20:20:13 +0300

    Backup after CBV

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -23,21 +23,21 @@ class BirthdayForm(forms.ModelForm):
             'birthday': forms.DateInput(attrs={'type': 'date'})
         }
         
-        def clean_first_name(self):
-            # Получаем значение имени из словаря очищенных данных.
-            first_name = self.cleaned_data['first_name']
-            # Разбиваем полученную строку по пробелам 
-            # и возвращаем только первое имя.
-            return first_name.split()[0]
+    def clean_first_name(self):
+        # Получаем значение имени из словаря очищенных данных.
+        first_name = self.cleaned_data['first_name']
+        # Разбиваем полученную строку по пробелам 
+        # и возвращаем только первое имя.
+        return first_name.split()[0]
         
-        def clean(self):
-            # Вызов родительского метода clean.
-            super().clean()
-            # Получаем имя и фамилию из очищенных полей формы.
-            first_name = self.cleaned_data['first_name']
-            last_name = self.cleaned_data['last_name']
-            # Проверяем вхождение сочетания имени и фамилии во множество имён.
-            if f'{first_name} {last_name}' in BEATLES:
-                raise ValidationError(
-                    'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
-                )
+    def clean(self):
+        # Вызов родительского метода clean.
+        super().clean()
+        # Получаем имя и фамилию из очищенных полей формы.
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        # Проверяем вхождение сочетания имени и фамилии во множество имён.
+        if f'{first_name} {last_name}' in BEATLES:
+            raise ValidationError(
+                'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+            )
